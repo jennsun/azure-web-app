914a526d08c2d718d1ae3df643e13f95debfe8aa
Jenny <jennysunnjm@gmail.com>
2023-10-14 12:55:46 -0400

    Serve the React App using Flask

--- app.py
+++ app.py
@@ -2,10 +2,14 @@ from flask import Flask, send_from_directory
 
 app = Flask(__name__, static_folder='client/build', static_url_path='/')
 
-@app.route('/')
-def hello_world():
-    return "Hello, World!"
+# @app.route('/')
+# def hello_world():
+#     return "Hello, World!"
 
-@app.route('/')
-def serve():
-    return send_from_directory(app.static_folder, 'index.html')
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists("client/build/" + path):
+        return send_from_directory('client/build', path)
+    else:
+        return send_from_directory('client/build', 'index.html')
