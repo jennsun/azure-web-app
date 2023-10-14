f3f239c6899f747dfaf0d812ea86760f10f7831a
Jenny <jennysunnjm@gmail.com>
2023-10-14 13:17:42 -0400

    Remove client so web app is flask app only

--- app.py
+++ app.py
@@ -1,15 +1,6 @@
-from flask import Flask, send_from_directory
+from flask import Flask
+app = Flask(__name__)
 
-app = Flask(__name__, static_folder='client/build', static_url_path='/')
-
-# @app.route('/')
-# def hello_world():
-#     return "Hello, World!"
-
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def serve(path):
-    if path != "" and os.path.exists("client/build/" + path):
-        return send_from_directory('client/build', path)
-    else:
-        return send_from_directory('client/build', 'index.html')
+@app.route('/')
+def hello():
+    return "Hello, World!"
