008368653c2fbb1f2158bbd9e21ad500c65b185b
Christine Sun <christinesunnjm@gmail.com>
2023-10-15 11:41:35 -0400

    Remove Flask files

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
-import configparser
 from azure.storage.blob import BlobServiceClient
 from fastapi import FastAPI, UploadFile, File
-from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
 import os
 
 app = FastAPI()
-origins = ["http://localhost:3000"]
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 # Configure Azure
 azure_connection_string = os.environ.get("AZURE_BLOB_CONNECTION_STRING")
