8291c6ea199f9bc3c3d9cda74c2328bd28c72b18
Christine Sun <christinesunnjm@gmail.com>
2023-10-15 11:10:01 -0400

    Add FastAPI base

---
+++ main.py
@@ -0,0 +1,55 @@
+import configparser
+from azure.storage.blob import BlobServiceClient
+from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+app = FastAPI()
+origins = ["http://localhost:3000"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Configure Azure
+config = configparser.ConfigParser()
+config.read("config.ini")
+azure_connection_string = config["azure"]["connection_string"]
+blob_service_client = BlobServiceClient.from_connection_string(azure_connection_string)
+INPUT_CONTAINER = "original-videos"
+OUTPUT_CONTAINER = "processed-videos"
+
+
+@app.get("/test/")
+def read_root():
+    """Simple hello world test"""
+    return {"Hello": "World"}
+
+
+@app.get("/test2/{value}")
+def manipulate_value(value):
+    """Add 1 to the given parameter"""
+    one_more = int(value) + 1
+    return {"val": one_more}
+
+
+@app.post("/upload/")
+async def upload_file(file: UploadFile = File(...)):
+    """Through the FastAPI endpoint, a user uploads a video. This video is saved to Azure Blob Storage in INPUT_CONTAINER. After the upload is complete, FastAPI triggers an Azure Batch job, sending it the location of the uploaded video in Blob Storage."""
+    blob_client = blob_service_client.get_blob_client(
+        container=INPUT_CONTAINER, blob=file.filename
+    )
+    print(f"blob client:{blob_client}")
+    blob_client.upload_blob(file.file.read())
+    # Here, you'd trigger the Azure Batch job for processing, passing the filename as a parameter
+    return {"filename": file.filename}
+
+
+@app.get("/processed/{filename}")
+async def get_processed_video(filename: str):
+    """Once processing is complete, FastAPI can serve the processed video back to the user, fetching it from the OUTPUT_CONTAINER in Azure Blob Storage."""
+    # Logic to fetch the processed video from Azure Blob and return to the user
+    pass
